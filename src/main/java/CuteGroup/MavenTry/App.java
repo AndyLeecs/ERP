1cc9ee975a87e27f02adb13f6f43c0897aa75c5a
Julia <925583000@qq.com>
2017-10-20 21:55:14 +0800

    ok

---
+++ src/main/java/CuteGroup/MavenTry/App.java
@@ -0,0 +1,13 @@
+package CuteGroup.MavenTry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
