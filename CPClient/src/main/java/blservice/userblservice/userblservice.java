19cada45e3f4f5d64d21addf211eceab64b5d30d
zxy <xiaozhang201012@sina.com>
2017-11-11 16:22:03 +0800

    增加messageVO作为系统中消息传递的值对象，修改userblservice的接口

--- CPClient/src/main/java/blservice/userblservice/userblservice.java
+++ CPClient/src/main/java/blservice/userblservice/userblservice.java
@@ -1,13 +1,31 @@
 package blservice.userblservice;
+import java.util.List;
+
+import VO.MessageVO;
+import VO.OperationVO;
 import VO.UserVO;
 import bl.utilitybl.ResultMessage;
 public interface userblservice {
 	
-    public ResultMessage login(String name, String password);//登录账户
-    public String create(UserVO vo);//创建账户
-    public String delete(String name);//删除账户
-    public String modify(UserVO vo); //更新账户信息
-    public UserVO userInfo(String id);//返回用户信息
+	/*
+	 * 每个用户所需的接口
+	 */
+    public ResultMessage login(String name, String password);					//登录账户
+    public ResultMessage changePassword(String oldPassword,String newPassword);	//修改密码
+    public List<MessageVO> checkMessage(String id);								//查看消息
     
-    
+    /*
+     * 系统管理员所需的接口
+     */
+    public String newUser(String name);											//创建新用户
+    public ResultMessage initAndSave(UserVO vo);								//保存新用户信息
+    public ResultMessage delete(String id);										//删除用户
+    public ResultMessage modify(UserVO vo);										//修改用户信息
+    public List<UserVO> findUser(String info);									//根据关键字查找用户
+
+    /*
+     * 操作日志相关的接口
+     */
+    public List<OperationVO> viewLog();											//查看操作日志
+    public ResultMessage saveOperation(OperationVO vo);							//生成操作日志
 }
