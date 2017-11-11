6c1b0ef212d2dc0268129087d9ba7c62eb914e92
zxy <xiaozhang201012@sina.com>
2017-11-11 21:25:24 +0800

    Revert "0"
    This reverts commit a883dc140259fcb8a62e09025e845380d1e66037.

--- CPServer/src/main/java/userdataservice/userdata_driver.java
+++ CPServer/src/main/java/userdataservice/userdata_driver.java
@@ -3,7 +3,7 @@ import PO.*;
 import userdata.*;
 public class userdata_driver {
        public void userdatadriver(){
-    	   UserPO p=new UserPO();
+    	   UserPO p=new UserPO("161250143","wangruihua","123456",3);
     	   userdata_stub us = new userdata_stub ();
     	   us.insert(p);
     	   us.find(p);
