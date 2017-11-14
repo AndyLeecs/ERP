3c850379d4e3b6d7e0e3f557bbd717a06b97ea58
161250055 <161250055@smail.nju.edu.cn>
2017-11-14 10:55:35 +0800

    删除data层冗余接口

--- CPServer/src/main/java/presentdataservice/PresentDataService.java
+++ CPServer/src/main/java/presentdataservice/PresentDataService.java
@@ -13,7 +13,25 @@ import PO.VIPGrade;
 * @date 2017年11月9日
 * @description
 */
-public interface PresentDataService extends PresentForAllDataService,PresentForMembershipDataService, PresentForSpecialPackageDataService,PresentForSumDataService{
+public interface PresentDataService {
+	public List<PresentPO> get();
 	
+	public String insert(PresentForMembershipPO po);
+	public boolean deletePresentForMembership(String id);
+	public boolean update(PresentForMembershipPO po);
+	public List<PresentForMembershipPO> getPresentForMembership();
+	public List<PresentForMembershipPO> getPresentForMembership(VIPGrade grade);
+	
+	public String insert(PresentForSpecialPackagePO po);
+	public boolean deletePresentForSpecialPackage(String id);
+	public boolean update(PresentForSpecialPackagePO po);
+	public List<PresentForSpecialPackagePO> getPresentForSpecialPackage();
+	public List<PresentForSpecialPackagePO> getPresentForSpecialPackage(List<String> GoodsID);
+	
+	public String insert(PresentForSumPO po);
+	public boolean deletePresentForSum(String id);
+	public boolean update(PresentForSumPO po);
+	public List<PresentForSumPO> getPresentForSum();
+	public List<PresentForSumPO> getPresentForSum(int sum);
 	
 }
--- CPServer/src/main/java/saledataservice/SaleDataService.java
+++ CPServer/src/main/java/saledataservice/SaleDataService.java
@@ -10,9 +10,38 @@ import PO.State;
 import PO.StockListPO;
 import PO.StockReturnListPO;
 
-public interface SaleDataService extends SaleForAllDataService, SaleListDataService, SaleReturnListDataService, StockListDataService,StockReturnListDataService{
+public interface SaleDataService {
+	public List<SalesmanListPO> get(State state);
 	
+	public String insert(SaleListPO po);
+	public boolean deleteSaleList(String id);
+	public boolean update(SaleListPO po);
+	public List<SaleListPO> getSaleList(State state);
+	
+	public String insert(SaleReturnListPO po);
+	public boolean deleteSaleReturnList(String id);
+	public boolean update(SaleReturnListPO po);
+	public List<SaleReturnListPO> getSaleReturnList(State state);
+	
+	public String insert(StockListPO po);
+	public boolean deleteStockList(String id);
+	public boolean update(StockListPO po);
+	public List<StockListPO> getStockList(State state);
+	
+	
+	public String insert(StockReturnListPO po);
 
+
+
+	public boolean deleteStockReturnList(String id);
+
+
+
+	public boolean update(StockReturnListPO po);
+
+
+
+	public List<StockReturnListPO> getStockReturnList(State state);
 
 
 }
