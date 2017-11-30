ad0913f41fdf9ce3d24ac62417db4612fe43d331
Julia <925583000@qq.com>
2017-11-30 09:32:34 +0800

    增加开场动画

--- CPClient/src/main/java/mainUI/Main.java
+++ CPClient/src/main/java/mainUI/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 public class Main extends Application
 {
 
-	public static void main(String[] args)
+	/*public static void main(String[] args)
 	{
 		launch(args);
-	}
+	}*/
 
 	@Override
 	public void start(Stage stage) throws Exception
