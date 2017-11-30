1421fa431f4fffb5386c6617d79b4e113a27380e
LAPTOP-AO7I50FN\lenovo <wangruihua88@yahoo.com>
2017-11-30 15:14:25 +0800

    表单的两个接口写好了。
    Signed-off-by: LAPTOP-AO7I50FN\lenovo <wangruihua88@yahoo.com>

---
+++ CPClient/src/main/java/VO/listVO/InfoListVO.java
@@ -0,0 +1,42 @@
+package VO.listVO;
+
+import util.ListType;
+
+public class InfoListVO {
+    private String id;  //单据编号
+    private ListType type;  // 单据类型
+    private String operater; //操作对象
+    private String note; //备注
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ListType getType() {
+        return type;
+    }
+
+    public void setType(ListType type) {
+        this.type = type;
+    }
+
+    public String getOperater() {
+        return operater;
+    }
+
+    public void setOperater(String operater) {
+        this.operater = operater;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
