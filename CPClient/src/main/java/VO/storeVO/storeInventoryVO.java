8afbc6e6ca4512fb2cbcd14c14a921f1be32c8c5
LAPTOP-AO7I50FN\lenovo <wangruihua88@yahoo.com>
2017-12-17 14:11:19 +0800

    提交两个文件，分别是库存查看和盘点的VO。

--- CPClient/src/main/java/VO/storeVO/storeInventoryVO.java
+++ CPClient/src/main/java/VO/storeVO/storeInventoryVO.java
@@ -1,4 +1,16 @@
 package VO.storeVO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class storeInventoryVO {
+    //库存盘点的值对象
+    //存储商品的名称、id、型号、库存数量、库存均价、出厂日期
+    public int N;//商品总数（其实这个变量是没有必要的）
+    public ArrayList<String> name ;
+    public ArrayList<String> id;
+    public ArrayList<String> model;//型号
+    public ArrayList<Integer> number;
+    public ArrayList<Double> averagePrice;
+    public ArrayList<String> time;
 }
