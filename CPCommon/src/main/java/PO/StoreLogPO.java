420cf381749a1fb15cb822a5a1a58e58d3af64bc
LAPTOP-AO7I50FN\lenovo <wangruihua88@yahoo.com>
2017-12-25 01:30:07 +0800

    祝大家圣诞快乐。

--- CPCommon/src/main/java/PO/StoreLogPO.java
+++ CPCommon/src/main/java/PO/StoreLogPO.java
@@ -5,9 +5,72 @@ import util.GreatListType;
 public class StoreLogPO {
 	//为了库存查看，必须记录下来每次库存变动的时间、商品ID、商品名称、变动原因、商品数量、涉及的单价
 	public String time;
-	public String id;
-	public String name;
-	public GreatListType type;
+    private GreatListType Type;
+
+    public GreatListType getType() {
+        return Type;
+    }
+
+    public void setType(GreatListType type) {
+        Type = type;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GreatListType type;
 	public int num;
-	public double price;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private String  autoId;
+
+    public String getAutoId() {
+        return autoId;
+    }
+
+    public void setAutoId(String autoId) {
+        this.autoId = autoId;
+    }
 }
--- CPCommon/src/main/java/PO/StorePO.java
+++ CPCommon/src/main/java/PO/StorePO.java
@@ -11,12 +11,64 @@ public class StorePO  implements Serializable{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 3819185728624176297L;
+	//private static final long serialVersionUID = 3819185728624176297L;
 	public String name; //商品名称
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String ID;  //商品ID
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String ID) {
+		this.ID = ID;
+	}
+
 	public int alarmNum;//警戒线
+
+	public int getAlarmNum() {
+		return alarmNum;
+	}
+
+	public void setAlarmNum(int alarmNum) {
+		this.alarmNum = alarmNum;
+	}
+
 	public int Num; //现有数量
-	
+
+	public int getNum() {
+		return Num;
+	}
+
+	public void setNum(int num) {
+		Num = num;
+	}
+
 	public double averagePrice;//库存均价
 
+	public double getAveragePrice() {
+		return averagePrice;
+	}
+
+	public void setAveragePrice(double averagePrice) {
+		this.averagePrice = averagePrice;
+	}
+
+	private String autoId;
+
+	public String getAutoId() {
+		return autoId;
+	}
+
+	public void setAutoId(String autoId ) {
+		this.autoId = autoId;
+	}
 }
