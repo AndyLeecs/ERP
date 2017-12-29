c11ab4bc5cbac7ea65d4e9fcea831bebb390e66d
LAPTOP-AO7I50FN\lenovo <wangruihua88@yahoo.com>
2017-12-29 13:45:47 +0800

    画了两个新的界面：（1）库存赠送单。（2）库存报损单/报溢单。  两个界面的逻辑还没写

---
+++ CPClient/src/main/java/ui/stockmanUI/ReportListController.java
@@ -0,0 +1,12 @@
+package ui.stockmanUI;
+
+public class ReportListController {
+
+	/*
+	 * 库存报溢单/报损单的Controller,有四种初始化状态
+	 * 第一种，在查看草稿单时打开
+	 * 第二种，在新建时打开
+	 * 第三种，在总经理查看审批时打开
+	 * 第四种，在财务人员查看审批时打开
+	 */
+}
